1efde0f1570a2a83d4cfb07f058c2a40f79699b3
Katsu <messyaikei456@gmail.com>
2022-12-20 13:48:06 +0530

    up

---
+++ assign5/q1.java
@@ -0,0 +1,18 @@
+package wut.assign5;
+
+public class  q1 {
+    public static void main(String[] args) {
+        
+        int n=20;
+        int count=0;
+       while(n>0){
+        count++;
+        n>>=1;
+    
+       }
+       System.out.println(count);
+    }
+   
+
+    
+}
